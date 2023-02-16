f09a42dcd292efe849f9455d3af4a75090f8fc08
HarshG04 <122627106+HarshG04@users.noreply.github.com>
2023-02-17 00:01:32 +0530

    Create isupper function.py

---
+++ isupper function.py
@@ -0,0 +1,8 @@
+# isupper function 
+# returns True if all the characters in the string are upper case,else returns false
+a = "HARSH GOYAL"
+b = "Harsh Goyal"
+c = "HARSH goyal"
+print(a.isupper())
+print(b.isupper())
+print(c.isupper())
